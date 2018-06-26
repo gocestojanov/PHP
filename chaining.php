c405fbe1848c7b587cd407e51d8f4338b984ff1e
Ivana Stojanova <ivana.kareva@gmail.com>
2018-06-26 13:41:36 +0200

    Chaining example

---
+++ chaining.php
@@ -0,0 +1,23 @@
+<?php
+
+class MyClass {
+
+  private $a = 1;
+  private $b = 1;
+
+  public function add(int $a) {
+    //$this->a += $a;
+    $b = $a+1;
+    return $this;
+  }
+
+  public function get() {
+    return 6;
+  }
+}
+
+
+$object = new MyClass();
+var_dump($object->add(4)->get());
+
+?>
